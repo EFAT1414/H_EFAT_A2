20b2ec715a597b4acb1a4d854249bc93958556a6
EFAT1414 <hasibulefat214@gmail.com>
2024-11-21 13:03:43 +0800

    Update person class

--- H_EFAT_A2/src/Person.java
+++ H_EFAT_A2/src/Person.java
@@ -1,17 +1,40 @@
 public  class Person{
-    protected String name;
-    protected  int age;
+    private  String name;
+    private  int age;
+    private String contactNumber;
+        //defalut constructor
+    public Person(){
+        this.name ="unknown";
+        this.age = 0;
+        this.contactNumber = "unknown";
+    }
 
-//Constructor
-    public Person(String name, int age){
+//Parametarized Constructor
+    public Person(String name, int age, String contactNumber){
         this.name = name;
         this.age = age;
+        this.contactNumber = contactNumber;
     }
-//print person information
+    //getter & setter
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public String getContactNumber(){
+        return contactNumber;
+    }
+    public void setContactNumber(String contactNumber){
+        this.contactNumber = contactNumber;
+    }
 
-    public void printInfo(){
-        System.out.println("Name :" + name);
-        System.out.println("Age :" + age);
-    }
+    
 
 }
