cfabc43b57bc5bf2c796a3ec461c2666d53db2bc
EFAT1414 <hasibulefat214@gmail.com>
2024-11-28 12:35:17 +0800

    Added default constructor

--- H_EFAT_A2/src/Ride.java
+++ H_EFAT_A2/src/Ride.java
@@ -1,3 +1,17 @@
 public class Ride {
 
+    private String rideName;
+    private int capacity;
+    private boolean isOpen;
+    private Employee operator; //employee assinged to operate the ride
+
+//default constructor
+    public Ride (){
+        this.rideName = "unknown";
+        this.capacity = 0;
+        this.isOpen = false;
+        this.operator = null;
+    }
+//parametarized constructor
+
 }
