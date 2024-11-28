d9f644f2f90c946c73aa2e10487a53cf37ac60e0
EFAT1414 <hasibulefat214@gmail.com>
2024-11-28 12:12:01 +0800

    Update employee class

--- H_EFAT_A2/src/Employee.java
+++ H_EFAT_A2/src/Employee.java
@@ -1,5 +1,35 @@
 public class Employee extends Person{
     private String jobTitle;
     private double  salary;
-}
 
+
+//dfefault constructor
+    public Employee(){
+        super();
+        this.jobTitle = "unknown";
+        this.salary = 0.0;
+    }
+
+    // parametarized constructor
+
+    public Employee(String name, int age, String contactNumber, String jobTitle, double salary){
+        super(name, age, contactNumber);
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+
+    }
+    //getter & setter
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public void setJobTitle(String jobTitle){
+        this.jobTitle = jobTitle;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public  void setSalary(double salary){
+        this.salary = salary;
+    }
+}
