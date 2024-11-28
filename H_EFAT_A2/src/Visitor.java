7b1095b60925a59b3c220577258207613ea7dd89
EFAT1414 <hasibulefat214@gmail.com>
2024-11-28 12:20:45 +0800

    Added constructor

--- H_EFAT_A2/src/Visitor.java
+++ H_EFAT_A2/src/Visitor.java
@@ -1,5 +1,17 @@
 public  class Visitor extends Person {
 //intense variable
+private String favouriteRide;
+private boolean hasSeasonalPass;
+
+//default constructor
+
+public Visitor(){
+    super();
+    this.favouriteRide = "unknown";
+    this.hasSeasonalPass = false;
+}
+
+
 
 
 }
