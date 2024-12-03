b98d54a88950d950fbc1bdfa4d46f7b7074db4f4
EFAT1414 <hasibulefat214@gmail.com>
2024-12-03 14:58:11 +0800

    create visitor

--- H_EFAT_A2/src/AssignmentTwo.java
+++ H_EFAT_A2/src/AssignmentTwo.java
@@ -3,6 +3,8 @@ public class AssignmentTwo {
        //create an Employee
         Employee operator = new Employee("Rotna", 32, "0424242498", "Student",1500);
 
-        
+        //create visitor
+        Visitor visitor1 = new Visitor("Manal",22,"044256987","Jet Ski",true);
+        Visitor visitor2 = new Visitor("Tamzid",23,"0421214321","Roller Coaster",true);
     }
 }    
