7fb2a92563b388aa0c3794d936f8f92a4d930fc0
EFAT1414 <hasibulefat214@gmail.com>
2024-12-03 14:53:42 +0800

    Create employee

--- H_EFAT_A2/src/AssignmentTwo.java
+++ H_EFAT_A2/src/AssignmentTwo.java
@@ -1,3 +1,8 @@
 public class AssignmentTwo {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+       //create an Employee
+        Employee operator = new Employee("Rotna", 32, "0424242498", "Student",1500);
+
+        
+    }
 }    
